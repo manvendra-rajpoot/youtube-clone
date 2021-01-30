e7db60d4a8b78c4e240899419991857197ac2001
manvendra-rajpoot <manvendra141986@gmail.com>
2021-01-30 20:48:13 +0530

    sidebar :setting

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -10,6 +10,13 @@ import PlayCircleOutlineRoundedIcon from '@material-ui/icons/PlayCircleOutlineRo
 import WatchLaterRoundedIcon from '@material-ui/icons/WatchLaterRounded';
 import ThumbUpRoundedIcon from '@material-ui/icons/ThumbUpRounded';
 import ExpandMoreRoundedIcon from '@material-ui/icons/ExpandMoreRounded';
+import SettingsRoundedIcon from '@material-ui/icons/SettingsRounded';
+import FlagRoundedIcon from '@material-ui/icons/FlagRounded';
+import HelpRoundedIcon from '@material-ui/icons/HelpRounded';
+import FeedbackRoundedIcon from '@material-ui/icons/FeedbackRounded';
+
+
+
 function Sidebar() {
     return (
         <div className='sidebar'>
@@ -25,6 +32,12 @@ function Sidebar() {
             <SidebarRow Icon={WatchLaterRoundedIcon} title='Watch later'/>
             <SidebarRow Icon={ThumbUpRoundedIcon} title='Liked videos'/>
             <SidebarRow Icon={ExpandMoreRoundedIcon} title='Show more'/>
+            <hr/>
+
+            <SidebarRow Icon={SettingsRoundedIcon} title='Setting'/>
+            <SidebarRow Icon={FlagRoundedIcon} title='Report history'/>
+            <SidebarRow Icon={HelpRoundedIcon} title='Help'/>
+            <SidebarRow Icon={FeedbackRoundedIcon} title='Send feedback'/>
             <hr/>
             
         </div>
